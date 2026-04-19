abd81279d7a057679d4025885ec27c533397f279
nissinissi2357-ship-it <nissinissi2357@gmail.com>
2026-04-20 00:26:52 +0900

    Force dynamic rendering for Vercel deploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import { AppShell } from "@/components/app-shell";
 import { getAppData, getViewerFromSession } from "@/lib/data";
 import { readSession } from "@/lib/session";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const session = await readSession();
   const [{ members, source, warning }, initialViewer] = await Promise.all([
